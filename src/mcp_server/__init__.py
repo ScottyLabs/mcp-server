7a363e3157f63a07e0a3e198ae13da1378ce6d7e
Parinthapat Pengpun <parinzee@protonmail.com>
2025-10-04 17:31:04 -0400

    feat: switch to streamable http transport

--- src/mcp_server/__init__.py
+++ src/mcp_server/__init__.py
@@ -1,2 +1,4 @@
-def main() -> None:
-    print("Hello from mcp-server!")
+from mcp_server.main import main_mcp
+
+if __name__ == "__main__":
+    main_mcp.run(transport="http", host="0.0.0.0", port=8000)
--- src/mcp_server/core/app.py
+++ src/mcp_server/core/app.py
@@ -1,3 +1,4 @@
 from fastmcp import FastMCP
+from fastmcp.client.transports import StreamableHttpTransport
 
 main_mcp = FastMCP(name="Scotty Labs MCPs for CMU", version="0.1.0")
